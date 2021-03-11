36401166a7e114bbd432b223c7ec4ec38554bd88
lioc <brenovisk7@gmail.com>
2021-03-11 18:08:52 -0300

    sending possibilities

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,30 @@
 class PagesController < ApplicationController
+    def explica()
+    end
+
     def index()
     end
+
+    def home1()
+        #Azul marinho com fonte branca
+    end
+
+    def home2()
+        #azul marinho com fonte cinza
+    end
+
+    def home3()
+        #Azul ciano com fonte cinza
+    end
+   
+    def home4()
+        # azul ciano com fonte azul marinho
+    end
+    
+    def home5()
+        # Branco com fonte azul marinho
+    end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  root to: 'pages#index'
+  # root to: 'pages#index'
+
+  root to: 'pages#explica'
+
+  get 'home1', to: 'pages#home1'
+  get 'home2', to: 'pages#home2'
+  get 'home3', to: 'pages#home3'
+  get 'home4', to: 'pages#home4'
+  get 'home5', to: 'pages#home5'
+
+  # verb 'path', to: 'controller#action'
 end
