d85daa0a46722fd6e1ca75ad46d8997bc305d7da
lioc <brenovisk7@gmail.com>
2021-03-15 14:50:12 -0300

    sending fixes on main page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,25 +6,10 @@ class PagesController < ApplicationController
     end
 
     def home1()
-        #Azul marinho com fonte branca
     end
 
     def home2()
-        #azul marinho com fonte cinza
     end
-
-    def home3()
-        #Azul ciano com fonte cinza
-    end
-   
-    def home4()
-        # azul ciano com fonte azul marinho
-    end
-    
-    def home5()
-        # Branco com fonte azul marinho
-    end
-
 
 
 end
